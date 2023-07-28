ecd08185dd49574c6798b0b8a23ab6ccf6e0a889
user <user@user.com>
2023-07-28 18:22:09 +0200

    Update PoliNetwork.Ai.Test.csproj, UnitTest1.cs, Usings.cs, and 67 more files

---
+++ PoliNetwork.Core.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PoliNetwork.Core.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
