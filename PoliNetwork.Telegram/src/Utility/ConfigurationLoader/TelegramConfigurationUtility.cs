02f87cdb67a6b662f47a235b4783ffa812f27a8d
angeousta <angeousta@outlook.it>
2023-09-01 11:33:18 +0200

    .

--- PoliNetwork.Telegram/src/Utility/ConfigurationLoader/TelegramConfigurationUtility.cs
+++ PoliNetwork.Telegram/src/Utility/ConfigurationLoader/TelegramConfigurationUtility.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using PoliNetwork.Telegram.ConfigurationLoader;
 
 namespace PoliNetwork.Telegram.Utility.ConfigurationLoader;
@@ -6,15 +6,7 @@ namespace PoliNetwork.Telegram.Utility.ConfigurationLoader;
 public class TelegramConfigurationUtility
 {
     public static readonly FileConfigurationLoader ConfigurationLoader = new JSONFileConfigurationLoader();
-=======
-using Newtonsoft.Json;
-using PoliNetwork.Telegram.Logger;
-using PoliNetwork.Telegram.Configuration;
 
-namespace PoliNetwork.Telegram.ConfigurationLoader;
 
-public class TelegramConfigurationUtility
-{
-    public static readonly FileConfigurationLoader configurationLoader = new JSONFileConfigurationLoader();
->>>>>>> v3
+
 }
